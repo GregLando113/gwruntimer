00b7c71b09a4734ccb27a8a04d8e0282459751d3
Greg Landowski <greglando@gmail.com>
2026-07-20 07:37:25 -0700

    in claude I trust o7

--- src/gw.rs
+++ src/gw.rs
@@ -4,18 +4,17 @@ use rusqlite::Map;
 use crate::memory::{Address,ProcessModule};
 use std::{sync::LazyLock, time::Duration};
 
-static GET_BUILD_NUMBER_FN: LazyLock<extern "cdecl" fn() -> u32> = LazyLock::new( || {
-    let result_fn = ProcessModule::main()
-        .expect("Cant get main module.")
-        .find_pattern("57 50 ?? ?? ?? ?? ?? ?? 56 ?? ?? ?? ?? ?? 83 C4 04 8B F0")
-        .expect("get_build_number_fn signature not found")
-        .add(0x14)
-        .deref_rel()
-        .expect("get_build_number_fn cannot follow call branch.");
-    unsafe { std::mem::transmute(result_fn.value()) }
-});
-
 pub fn get_build_number() -> u32 {
+    static GET_BUILD_NUMBER_FN: LazyLock<extern "cdecl" fn() -> u32> = LazyLock::new(|| {
+        let result_fn = ProcessModule::main()
+            .expect("Cant get main module.")
+            .find_pattern("57 50 ?? ?? ?? ?? ?? ?? 56 ?? ?? ?? ?? ?? 83 C4 04 8B F0")
+            .expect("get_build_number_fn signature not found")
+            .add(0x14)
+            .deref_rel()
+            .expect("get_build_number_fn cannot follow call branch.");
+        unsafe { std::mem::transmute(result_fn.value()) }
+    });
     GET_BUILD_NUMBER_FN()
 }
 
